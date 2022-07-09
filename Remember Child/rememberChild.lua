3ff876aa7495059beb84fec8f699fa87be8c766d
Pat Maddox <pat@patmaddox.com>
2022-07-08 23:37:45 -0700

    Default to remembering first child if no child can be found

--- Remember Child/rememberChild.lua
+++ Remember Child/rememberChild.lua
@@ -21,15 +21,36 @@ end
 function restoreChild()
   retVal, childId = reaper.GetProjExtState(0, extName, selectedId)
 
+  foundChild = false
+
   if childId ~= "" then
     for trackIndex = 0, reaper.CountTracks(0) - 1 do
       track = reaper.GetTrack(0, trackIndex)
 
       if childId == reaper.GetTrackGUID(track) then
+        foundChild = true
         reaper.SetTrackSelected(selectedTrack, false)
         reaper.SetTrackSelected(track, true)
         break
       end
     end
+  end
+
+  if not foundChild then
+    for trackIndex = 0, reaper.CountTracks(0) - 1 do
+      track = reaper.GetTrack(0, trackIndex)
+
+      if reaper.GetParentTrack(track) == selectedTrack then
+        foundChild = true
+        reaper.SetTrackSelected(selectedTrack, false)
+        reaper.SetTrackSelected(track, true)
+        break
+      end
+    end
+  end
+
+  if not foundChild then
+    reaper.ShowMessageBox("Didn't find a child", "Error", 0)
+    os.exit()
   end
 end
